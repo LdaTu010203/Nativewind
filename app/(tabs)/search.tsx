4793257affc740a711699ffb5c6a4e4a3e671235
Tú Lê <leduonganhtu0102@gmail.com>
2024-07-12 23:20:23 +0700

    build 6

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,4 +1,10 @@
-import { View, Text, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import React from "react";
 
 const Search = () => {
@@ -7,12 +13,66 @@ const Search = () => {
       <Text className="font-PolyRegular text-yellow-100 text-4xl pb-5 pt-10">
         Search
       </Text>
-      <View className="flex-1 self-center ">
-        <TextInput
-          placeholder="Search"
-          className="bg-gray-500 placeholder:font-PolyRegular text-sm text-black rounded-full justify-stretch h-10 px-44"
-        ></TextInput>
-      </View>
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        <View className="flex-1 justify-stretch flex-row gap-5">
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">
+              Hoodies
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">
+              Jackets
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">Shirt</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">
+              Sweater
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">
+              T-Shirts
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">
+              Shorts
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">Pants</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">
+              Bags & Wallets
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">
+              Footwear
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">
+              Slippers
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">
+              Phone Case
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-yellow-100 w-24 h-12 items-center justify-center rounded-2xl">
+            <Text className="text-base font-PolyRegular text-black">
+              Cap & Snapback
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
 };
--- app/(tabs)/shop.tsx
+++ app/(tabs)/shop.tsx
@@ -1,12 +1,37 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableHighlight,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React from "react";
 
-const shop = () => {
+const Shop = () => {
   return (
-    <View>
-      <Text>shop</Text>
+    <View className="flex-1 bg-gray-700 justify-center items-center pt-10">
+      <Text className="font-PolyRegular text-yellow-100 text-4xl pb-5 pt-10">
+        Shop
+      </Text>
+      <ScrollView className="flex-1 mb-12" showsVerticalScrollIndicator={false}>
+        <View className="flex-col flex-1 justify-stretch">
+          <View className="flex-row self-stretch gap-1">
+            <TouchableOpacity className="w-48 h-80 bg-red-500">
+              <Image
+                source={require("@/assets/images/Samurai.png")}
+                className="w-48 h-52"
+              />
+              <Text className="text-base font-PolyRegular text-yellow-100 self-center">
+                Samurai
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="w-48 h-80 bg-red-500"></TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
-export default shop;
+export default Shop;
